4955792854d55b8762251c14a2e14604661c55a9
Pierig Le Saux <lesaux@pythian.com>
2014-12-02 15:42:51 -0500

    navisphere security file creation

--- manifests/emc.pp
+++ manifests/emc.pp
@@ -1,7 +1,8 @@
-class sensucustom::emc (
-
-$destination = '/etc/sensu/plugins',
-
+class sensucustom::emc ( 
+  $destination = '/etc/sensu/plugins',
+  $username    = 'username',
+  $password    = 'password',
+  $scope       = 1,
 ) {
 
   file { "${destination}/check_emc_clariion.pl":
@@ -10,6 +11,20 @@ $destination = '/etc/sensu/plugins',
         owner   => sensu,
         group   => sensu,
         mode    => 0755,
+  }->
+
+  file { "${destination}/check_emc_clariion_security_files":
+    ensure  => directory,
+    recurse => true,
+    source  => 'puppet:///modules/sensucustom/sensuscripts/modules',
+    owner   => sensu,
+    group   => sensu,
+    mode    => '0600',
+  }->
+
+  exec {'emc_security_file':
+    command => '/opt/Navisphere/bin/naviseccli -secfilepath $destination/check_emc_clariion_security_files -User $username -Password $password -Scope $scope -AddUserSecurity',
+    unless  => '/usr/bin/test -f $destination/check_emc_clariion_security_files/SecuredCLISecurityFile.xml',
   }
 
 }
@@ -17,8 +32,8 @@ $destination = '/etc/sensu/plugins',
 
 define sensucustom::emc::check ( $ip, $entity, $username, $password  ) {
 
-    sensu::check { "check_emc_sp_${host}":
-      command     => "/etc/sensu/plugins/check_emc_clariion.pl -H ${ip} -u ${username} -p ${password} -t sp",
+    sensu::check { "check_emc_faults_${host}":
+      command     => "/etc/sensu/plugins/check_emc_clariion.pl -H ${ip} -u ${username} -p ${password} -t faults",
       subscribers => 'remote_emc',
       handlers    => ['flapjack'],
       standalone  =>  false,
@@ -28,5 +43,67 @@ define sensucustom::emc::check ( $ip, $entity, $username, $password  ) {
         source => "${entity}"
       }
     }
+
+    sensu::check { "check_emc_disks_${host}":
+      command     => "/etc/sensu/plugins/check_emc_clariion.pl -H ${ip} -u ${username} -p ${password} -t disk",
+      subscribers => 'remote_emc',
+      handlers    => ['flapjack'],
+      standalone  =>  false,
+      type        => 'metric',
+      interval    => '1800',
+      custom => {
+        source => "${entity}"
+      }
+    }
+
+    sensu::check { "check_emc_cache_${host}":
+      command     => "/etc/sensu/plugins/check_emc_clariion.pl -H ${ip} -u ${username} -p ${password} -t cache",
+      subscribers => 'remote_emc',
+      handlers    => ['flapjack'],
+      standalone  =>  false,
+      type        => 'metric',
+      interval    => '1800',
+      custom => {
+        source => "${entity}"
+      }
+    }
+
+    sensu::check { "check_emc_cache_${host}":
+      command     => "/etc/sensu/plugins/check_emc_clariion.pl -H ${ip} -u ${username} -p ${password} -t cache_pdp",
+      subscribers => 'remote_emc',
+      handlers    => ['flapjack'],
+      standalone  =>  false,
+      type        => 'metric',
+      interval    => '1800',
+      custom => {
+        source => "${entity}"
+      }
+    }
+
+
+    sensu::check { "check_emc_spA_${host}":
+      command     => "/etc/sensu/plugins/check_emc_clariion.pl -H ${ip} -u ${username} -p ${password} -t sp --sp A",
+      subscribers => 'remote_emc',
+      handlers    => ['flapjack'],
+      standalone  =>  false,
+      type        => 'metric',
+      interval    => '1800',
+      custom => {
+        source => "${entity}"
+      }
+    }
+
+    sensu::check { "check_emc_spB_${host}":
+      command     => "/etc/sensu/plugins/check_emc_clariion.pl -H ${ip} -u ${username} -p ${password} -t sp --sp B",
+      subscribers => 'remote_emc',
+      handlers    => ['flapjack'],
+      standalone  =>  false,
+      type        => 'metric',
+      interval    => '1800',
+      custom => {
+        source => "${entity}"
+      }
+    }
+
 }
 

4e702dcaa4b776bdab144db3b0435bf0167ae9c0
pythianemord <emord@pythian.com>
2016-01-25 11:09:07 -0500

    Be even more tolerant of Graphite client restarts....

--- manifests/subscriptions/windows-graphite.pp
+++ manifests/subscriptions/windows-graphite.pp
@@ -13,7 +13,7 @@ class sensucustom::subscriptions::windows-graphite (
     },
   }
   sensu::check { 'check_graphite_windows_disk_percent_free_space':
-    command     => "/pythian/sensu/checks/check-data.bat ${graphite_host} disk.percent_free_space_* \":::params.graphite.windows.disk.percent.free.warning|10:::\" \":::params.graphite.windows.disk.percent.free.critical|5:::\" -ba90",
+    command     => "/pythian/sensu/checks/check-data.bat ${graphite_host} disk.percent_free_space_* \":::params.graphite.windows.disk.percent.free.warning|10:::\" \":::params.graphite.windows.disk.percent.free.critical|5:::\" -ba130",
     subscribers => 'windows_graphite',
     standalone  =>  false,
     type        => 'metric',
@@ -23,49 +23,49 @@ class sensucustom::subscriptions::windows-graphite (
     },
   }
   sensu::check { 'check_graphite_windows_network_packets_discarded':
-    command     => "/pythian/sensu/checks/check-data.bat ${graphite_host} network.packets_*_discarded \":::params.graphite.windows.network.packets.discarded.warning|1:::\" \":::params.graphite.raphite.windows.network.packets.discarded..critical|1:::\" -a90",
+    command     => "/pythian/sensu/checks/check-data.bat ${graphite_host} network.packets_*_discarded \":::params.graphite.windows.network.packets.discarded.warning|1:::\" \":::params.graphite.raphite.windows.network.packets.discarded..critical|1:::\" -a130",
     subscribers => 'windows_graphite',
     standalone  =>  false,
     type        => 'metric',
     handlers    => ['flapjack'],
   }
   sensu::check { 'check_graphite_windows_network_packets_errors':
-    command     => "/pythian/sensu/checks/check-data.bat ${graphite_host} network.packets_*_errors \":::params.graphite.windows.network.packets.discarded.warning|1:::\" \":::params.graphite.windows.network.packets.discarded.critical|1:::\" -a90",
+    command     => "/pythian/sensu/checks/check-data.bat ${graphite_host} network.packets_*_errors \":::params.graphite.windows.network.packets.discarded.warning|1:::\" \":::params.graphite.windows.network.packets.discarded.critical|1:::\" -a130",
     subscribers => 'windows_graphite',
     standalone  =>  false,
     type        => 'metric',
     handlers    => ['flapjack'],
   }
   sensu::check { 'check_graphite_windows_threads':
-    command     => "/pythian/sensu/checks/check-data.bat ${graphite_host} system.threads \":::params.graphite.windows.system.threads.warning|3000:::\" \":::params.graphite.windows.system.threads.critical|4000:::\" -a90",
+    command     => "/pythian/sensu/checks/check-data.bat ${graphite_host} system.threads \":::params.graphite.windows.system.threads.warning|3000:::\" \":::params.graphite.windows.system.threads.critical|4000:::\" -a130",
     subscribers => 'windows_graphite',
     standalone  =>  false,
     type        => 'metric',
     handlers    => ['flapjack'],
   }
   sensu::check { 'check_graphite_windows_processor_queue_length':
-    command     => "/pythian/sensu/checks/check-data.bat ${graphite_host} system.processor_queue_length \":::params.graphite.windows.system.processor.queue.length.warning|40:::\" \":::params.graphite.windows.system.processor.queue.length.critical|50:::\" -a90",
+    command     => "/pythian/sensu/checks/check-data.bat ${graphite_host} system.processor_queue_length \":::params.graphite.windows.system.processor.queue.length.warning|40:::\" \":::params.graphite.windows.system.processor.queue.length.critical|50:::\" -a130",
     subscribers => 'windows_graphite',
     standalone  =>  false,
     type        => 'metric',
     handlers    => ['flapjack'],
   }
   sensu::check { 'check_graphite_windows_processes':
-    command     => "/pythian/sensu/checks/check-data.bat ${graphite_host} system.processes \":::params.graphite.windows.system.processes.warning|700:::\" \":::params.graphite.windows.system.processes.critical|1000:::\" -a90",
+    command     => "/pythian/sensu/checks/check-data.bat ${graphite_host} system.processes \":::params.graphite.windows.system.processes.warning|700:::\" \":::params.graphite.windows.system.processes.critical|1000:::\" -a130",
     subscribers => 'windows_graphite',
     standalone  =>  false,
     type        => 'metric',
     handlers    => ['flapjack'],
   }
   sensu::check { 'check_graphite_windows_file_data_ops':
-    command     => "/pythian/sensu/checks/check-data.bat ${graphite_host} system.file_data_ops_sec \":::params.graphite.windows.system.file.data.ops.warning|15000:::\" \":::params.graphite.windows.system.file.data.ops.critical|20000:::\" -a90",
+    command     => "/pythian/sensu/checks/check-data.bat ${graphite_host} system.file_data_ops_sec \":::params.graphite.windows.system.file.data.ops.warning|15000:::\" \":::params.graphite.windows.system.file.data.ops.critical|20000:::\" -a130",
     subscribers => 'windows_graphite',
     standalone  =>  false,
     type        => 'metric',
     handlers    => ['flapjack'],
   }
   sensu::check { 'check_graphite_windows_processor_interrupts':
-    command     => "/pythian/sensu/checks/check-data.bat ${graphite_host} processor.interrupts_sec \":::params.graphite.windows.processor.interrupts.warning|15000:::\" \":::params.graphite.windows.processor.interrupts.critical|20000:::\" -a90",
+    command     => "/pythian/sensu/checks/check-data.bat ${graphite_host} processor.interrupts_sec \":::params.graphite.windows.processor.interrupts.warning|15000:::\" \":::params.graphite.windows.processor.interrupts.critical|20000:::\" -a130",
     subscribers => 'windows_graphite',
     standalone  =>  false,
     type        => 'metric',

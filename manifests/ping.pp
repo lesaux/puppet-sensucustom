c1db830f70b340e01188b695422605cc04e51c9f
pythianemord <emord@pythian.com>
2016-02-18 18:48:50 -0500

    Update ping ratios to reduce transient alerts
    The logic within the check-ping.rb script looks for the success ratio to be > the warning ratio configured, not >=, so a single dropped packet out of 10 would generate a success ratio of .9 which is not greater than the previously configured .9 warning threshold and therefore would generate a warning.
    
    successful_count = result.count(true)
    total_count = config[:count]
    success_ratio = successful_count / total_count.to_f
    
    if success_ratio > config[:warn_ratio]
    	success_message = "ICMP ping successful for host: #{config[:host]}"
    	ok success_message
    else
    	failure_message = "ICMP ping unsuccessful for host: #{config[:host]} (successful: #{successful_count}/#{total_count})"
    
    	if config[:report]
    		report = `mtr --curses --report-cycles=1 --report --no-dns #{config[:host]}`
    		failure_message = failure_message + "\n" + report
    	end
    
    	if success_ratio <= config[:critical_ratio]
    		critical failure_message
    	else
    		warning failure_message
    	end
    end

--- manifests/ping.pp
+++ manifests/ping.pp
@@ -1,7 +1,7 @@
 
 define sensucustom::ping  {
   sensu::check { "check_ping_${name}":
-    command     => "/etc/sensu/plugins/check-ping.rb -h ${name} -c 10 -C 0.8 -W 0.9 -i 1 -T 5",
+    command     => "/etc/sensu/plugins/check-ping.rb -h ${name} -c 10 -C 0.7 -W 0.8 -i 1 -T 5",
     handlers    => ['flapjack'],
     subscribers => 'remote_ping',
     standalone  =>  false,
